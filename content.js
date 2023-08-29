023cfc8018eab3138bf25b8517b99ddbea2f5e0c
mak66 <mak66@users.noreply.github.com>
2023-08-29 20:46:03 +0100

    Added shortcut and direct option
    Added keyboard shortcut for actions as well as the option to paste direct to current field

--- content.js
+++ content.js
@@ -1,13 +1,19 @@
 chrome.runtime.onMessage.addListener( // this is the message listener
     function (request, sender, sendResponse) {
         console.log('request', request)
-        if (request.message === "copyText")
+        if (request.message === "directText") {
+            insertToCurrentElement(request.textToCopy);
+        }
+        else if (request.message === "copyText") {
             copyToTheClipboard(request.textToCopy);
+        }
         sendResponse();
     }
 );
 
 async function copyToTheClipboard(textToCopy) {
+    // keep the current element so we can return focus
+    const current = document.activeElement;
     const el = document.createElement('textarea');
     el.value = textToCopy;
     el.setAttribute('readonly', '');
@@ -17,4 +23,14 @@ async function copyToTheClipboard(textToCopy) {
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
+    // return focus to current element
+    document.getElementById(current.id).focus();
+}
+
+function insertToCurrentElement(textToCopy) {
+    // keep the current element so we can return focus
+    const currentElement = document.activeElement;
+    const newValue = currentElement.value + textToCopy;
+    // return focus to current element
+    currentElement.value = newValue;
 }
